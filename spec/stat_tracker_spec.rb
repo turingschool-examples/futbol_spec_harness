42c07ea815a5ccfcaf9cccdc0fa2ecad1d55681b
memcmahon <mcmahon.meganelizabeth@gmail.com>
2020-04-05 20:34:38 -0600

    Update for refocused futbol project

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -27,10 +27,6 @@ RSpec.describe StatTracker do
     expect(@stat_tracker.lowest_total_score).to eq 0
   end
 
-  it "#biggest_blowout" do
-    expect(@stat_tracker.biggest_blowout).to eq 8
-  end
-
   it "#percentage_home_wins" do
     expect(@stat_tracker.percentage_home_wins).to eq 0.44
   end
@@ -83,14 +79,6 @@ RSpec.describe StatTracker do
     expect(@stat_tracker.worst_offense).to eq "Utah Royals FC"
   end
 
-  it "#best_defense" do
-    expect(@stat_tracker.best_defense).to eq "FC Cincinnati"
-  end
-
-  it "#worst_defense" do
-    expect(@stat_tracker.worst_defense).to eq "Columbus Crew SC"
-  end
-
   it "#highest_scoring_visitor" do
     expect(@stat_tracker.highest_scoring_visitor).to eq "FC Dallas"
   end
@@ -105,18 +93,6 @@ RSpec.describe StatTracker do
 
   it "#lowest_scoring_home_team" do
     expect(@stat_tracker.lowest_scoring_home_team).to eq "Utah Royals FC"
-  end
-
-  it "#winningest_team" do
-    expect(@stat_tracker.winningest_team).to eq "Reign FC"
-  end
-
-  it "#best_fans" do
-    expect(@stat_tracker.best_fans).to eq "San Jose Earthquakes"
-  end
-
-  it "#worst_fans" do
-    expect(@stat_tracker.worst_fans).to eq ["Houston Dynamo", "Utah Royals FC"]
   end
 
   it "#team_info" do
@@ -159,206 +135,33 @@ RSpec.describe StatTracker do
     expect(@stat_tracker.rival("18")).to eq("Houston Dash").or(eq("LA Galaxy"))
   end
 
-  it "#biggest_team_blowout" do
-    expect(@stat_tracker.biggest_team_blowout("18")).to eq 5
+  it "#winningest_coach" do
+    expect(@stat_tracker.winningest_coach("20132014")).to eq "Claude Julien"
+    expect(@stat_tracker.winningest_coach("20142015")).to eq "Alain Vigneault"
   end
 
-  it "#worst_loss" do
-    expect(@stat_tracker.worst_loss("18")).to eq 4
+  it "#worst_coach" do
+    expect(@stat_tracker.worst_coach("20132014")).to eq "Peter Laviolette"
+    expect(@stat_tracker.worst_coach("20142015")).to eq("Craig MacTavish").or(eq("Ted Nolan"))
   end
 
-  it "#head_to_head" do
-    expected_1 = {
-     "Atlanta United"=>0.5,
-     "Chicago Fire"=>0.3,
-     "FC Cincinnati"=>0.39,
-     "DC United"=>0.8,
-     "FC Dallas"=>0.4,
-     "Houston Dynamo"=>0.4,
-     "Sporting Kansas City"=>0.25,
-     "LA Galaxy"=>0.29,
-     "Los Angeles FC"=>0.44,
-     "Montreal Impact"=>0.33,
-     "New England Revolution"=>0.47,
-     "New York City FC"=>0.6,
-     "New York Red Bulls"=>0.4,
-     "Orlando City SC"=>0.37,
-     "Portland Timbers"=>0.3,
-     "Philadelphia Union"=>0.44,
-     "Real Salt Lake"=>0.42,
-     "San Jose Earthquakes"=>0.33,
-     "Seattle Sounders FC"=>0.5,
-     "Toronto FC"=>0.33,
-     "Vancouver Whitecaps FC"=>0.44,
-     "Chicago Red Stars"=>0.48,
-     "Houston Dash"=>0.1,
-     "North Carolina Courage"=>0.2,
-     "Orlando Pride"=>0.47,
-     "Portland Thorns FC"=>0.45,
-     "Reign FC"=>0.33,
-     "Sky Blue FC"=>0.3,
-     "Utah Royals FC"=>0.6,
-     "Washington Spirit FC"=>0.67,
-     "Columbus Crew SC"=>0.5
-    }
-    
-    expected_2 = {
-      "Atlanta United" => 0.6,
-      "Chicago Fire" => 0.8,
-      "Chicago Red Stars" => 0.63,
-      "Columbus Crew SC" => 0.75,
-      "DC United" => 1.0,
-      "FC Cincinnati" => 0.56,
-      "FC Dallas" => 0.7,
-      "Houston Dash" => 0.4,
-      "Houston Dynamo" => 0.7,
-      "LA Galaxy" => 0.36,
-      "Los Angeles FC" => 0.56,
-      "Montreal Impact" => 0.61,
-      "New England Revolution" => 0.63,
-      "New York City FC" => 0.8,
-      "New York Red Bulls" => 0.7,
-      "North Carolina Courage" => 0.5,
-      "Orlando City SC" => 0.59,
-      "Orlando Pride" => 0.6,
-      "Philadelphia Union" => 0.56,
-      "Portland Thorns FC" => 0.55,
-      "Portland Timbers" => 0.5,
-      "Real Salt Lake" => 0.74,
-      "Reign FC" => 0.67,
-      "San Jose Earthquakes" => 0.67,
-      "Seattle Sounders FC" => 0.6,
-      "Sky Blue FC" => 0.6,
-      "Sporting Kansas City" => 0.44,
-      "Toronto FC" => 0.61,
-      "Utah Royals FC" => 0.7,
-      "Vancouver Whitecaps FC" => 0.63,
-      "Washington Spirit FC" => 0.78,
-    }
-
-    expect(@stat_tracker.head_to_head("18")).to eq(expected_1).or(eq(expected_2))
+  it "#most_accurate_team" do
+    expect(@stat_tracker.most_accurate_team("20132014")).to eq "Real Salt Lake"
+    expect(@stat_tracker.most_accurate_team("20142015")).to eq "Toronto FC"
   end
 
-  it "#seasonal_summary" do
-    expected = {"20162017"=>
-        {:postseason=>
-          {:win_percentage=>0.59,
-           :total_goals_scored=>48,
-           :total_goals_against=>40,
-           :average_goals_scored=>2.18,
-           :average_goals_against=>1.82},
-         :regular_season=>
-          {:win_percentage=>0.38,
-           :total_goals_scored=>180,
-           :total_goals_against=>170,
-           :average_goals_scored=>2.2,
-           :average_goals_against=>2.07}},
-       "20172018"=>
-        {:postseason=>
-          {:win_percentage=>0.54,
-           :total_goals_scored=>29,
-           :total_goals_against=>28,
-           :average_goals_scored=>2.23,
-           :average_goals_against=>2.15},
-         :regular_season=>
-          {:win_percentage=>0.44,
-           :total_goals_scored=>187,
-           :total_goals_against=>162,
-           :average_goals_scored=>2.28,
-           :average_goals_against=>1.98}},
-       "20132014"=>
-        {:postseason=>
-          {:win_percentage=>0.0,
-           :total_goals_scored=>0,
-           :total_goals_against=>0,
-           :average_goals_scored=>0.0,
-           :average_goals_against=>0.0},
-         :regular_season=>
-          {:win_percentage=>0.38,
-           :total_goals_scored=>166,
-           :total_goals_against=>177,
-           :average_goals_scored=>2.02,
-           :average_goals_against=>2.16}},
-       "20122013"=>
-        {:postseason=>
-          {:win_percentage=>0.0,
-           :total_goals_scored=>0,
-           :total_goals_against=>0,
-           :average_goals_scored=>0.0,
-           :average_goals_against=>0.0},
-         :regular_season=>
-          {:win_percentage=>0.25,
-           :total_goals_scored=>85,
-           :total_goals_against=>103,
-           :average_goals_scored=>1.77,
-           :average_goals_against=>2.15}},
-       "20142015"=>
-        {:postseason=>
-          {:win_percentage=>0.67,
-           :total_goals_scored=>17,
-           :total_goals_against=>13,
-           :average_goals_scored=>2.83,
-           :average_goals_against=>2.17},
-         :regular_season=>
-          {:win_percentage=>0.5,
-           :total_goals_scored=>186,
-           :total_goals_against=>162,
-           :average_goals_scored=>2.27,
-           :average_goals_against=>1.98}},
-       "20152016"=>
-        {:postseason=>
-          {:win_percentage=>0.36,
-           :total_goals_scored=>25,
-           :total_goals_against=>33,
-           :average_goals_scored=>1.79,
-           :average_goals_against=>2.36},
-         :regular_season=>
-          {:win_percentage=>0.45,
-           :total_goals_scored=>178,
-           :total_goals_against=>159,
-           :average_goals_scored=>2.17,
-           :average_goals_against=>1.94}}}
+  it "#least_accurate_team" do
+    expect(@stat_tracker.least_accurate_team("20132014")).to eq "New York City FC"
+    expect(@stat_tracker.least_accurate_team("20142015")).to eq "Columbus Crew SC"
+  end
 
-      expect(@stat_tracker.seasonal_summary("18")).to eq expected
-    end
+  it "#most_tackles" do
+    expect(@stat_tracker.most_tackles("20132014")).to eq "FC Cincinnati"
+    expect(@stat_tracker.most_tackles("20142015")).to eq "Seattle Sounders FC"
+  end
 
-    it "#biggest_bust" do
-      expect(@stat_tracker.biggest_bust("20132014")).to eq "Montreal Impact"
-      expect(@stat_tracker.biggest_bust("20142015")).to eq "Sporting Kansas City"
-    end
-
-    it "#biggest_surprise" do
-      expect(@stat_tracker.biggest_surprise("20132014")).to eq "FC Cincinnati"
-      expect(@stat_tracker.biggest_surprise("20142015")).to eq "Minnesota United FC"
-    end
-
-    it "#winningest_coach" do
-      expect(@stat_tracker.winningest_coach("20132014")).to eq "Claude Julien"
-      expect(@stat_tracker.winningest_coach("20142015")).to eq "Alain Vigneault"
-    end
-
-    it "#worst_coach" do
-      expect(@stat_tracker.worst_coach("20132014")).to eq "Peter Laviolette"
-      expect(@stat_tracker.worst_coach("20142015")).to eq("Craig MacTavish").or(eq("Ted Nolan"))
-    end
-
-    it "#most_accurate_team" do
-      expect(@stat_tracker.most_accurate_team("20132014")).to eq "Real Salt Lake"
-      expect(@stat_tracker.most_accurate_team("20142015")).to eq "Toronto FC"
-    end
-
-    it "#least_accurate_team" do
-      expect(@stat_tracker.least_accurate_team("20132014")).to eq "New York City FC"
-      expect(@stat_tracker.least_accurate_team("20142015")).to eq "Columbus Crew SC"
-    end
-
-    it "#most_tackles" do
-      expect(@stat_tracker.most_tackles("20132014")).to eq "FC Cincinnati"
-      expect(@stat_tracker.most_tackles("20142015")).to eq "Seattle Sounders FC"
-    end
-
-    it "#fewest_tackles" do
-      expect(@stat_tracker.fewest_tackles("20132014")).to eq "Atlanta United"
-      expect(@stat_tracker.fewest_tackles("20142015")).to eq "Orlando City SC"
-    end
+  it "#fewest_tackles" do
+    expect(@stat_tracker.fewest_tackles("20132014")).to eq "Atlanta United"
+    expect(@stat_tracker.fewest_tackles("20142015")).to eq "Orlando City SC"
+  end
 end
